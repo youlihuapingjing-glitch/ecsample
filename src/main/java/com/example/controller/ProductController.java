372ebb030753d0a816d28b3c1625b7618af05627
Yurika Hirai <youlihuapingjing@gmail.com>
2026-04-28 12:01:25 +0900

    商品詳細ページを追加（PathVariableの値を取得してコンソール出力）

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProductController {
@@ -10,5 +11,14 @@ public class ProductController {
 	public String showList() {
 		// 商品一覧ページを表示する
 		return "product/list";
+	}
+
+	@GetMapping("/product/{id}")
+	public String showDetail(@PathVariable("id") int id) {
+		// URL で指定された id を受け取る
+		System.out.println("アクセスされたID：" + id);
+
+		// 商品詳細ページを表示する
+		return "product/detail";
 	}
 }
