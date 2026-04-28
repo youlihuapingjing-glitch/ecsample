be2f811bb4664193d9b09dea7550d0b2991a460d
Yurika Hirai <youlihuapingjing@gmail.com>
2026-04-28 11:44:03 +0900

    トップページと商品一覧ページを表示するController・テンプレートを追加

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class ProductController {
 
-	@GetMapping("/")
-	public String home() {
-		// templates/home.html を表示する
-		return "home";
+	@GetMapping("/products")
+	public String showList() {
+		// 商品一覧ページを表示する
+		return "product/list";
 	}
 }
